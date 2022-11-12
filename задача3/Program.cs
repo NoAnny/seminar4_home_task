158bb7feeaf8b8620fb2fd4da1b343cb276e133c
NoAnnY <noanny@mail.ru>
2022-11-13 00:55:30 +0300

    ready

---
+++ задача3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] RandomArray(int n)
+{
+    int [] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        System.Console.Write(array[i]);
+    }
+    return array;
+}
+
+System.Console.WriteLine("Введите длину массива");
+int n = int.Parse(Console.ReadLine()!);
+RandomArray(n);
