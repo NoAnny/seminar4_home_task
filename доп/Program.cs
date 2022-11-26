1430312e59476394227bd37f40ecebd0640d78af
NoAnnY <noanny@mail.ru>
2022-11-26 16:47:30 +0300

    решение 1

---
+++ доп/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выдов простых чисел от 1 до N
+System.Console.WriteLine("Введите число N:");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine ($"Ряд простых чисел от 1 до {n}:");
+for (int current = 2; current <= n; current++)
+{
+    bool isPrime = true;
+    for (int i = 2; i < current; i++)
+    {
+        if (current % i == 0) isPrime = false;
+        break;
+    }
+    if (isPrime) Console.Write(current + " "); 
+}
